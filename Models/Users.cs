5248ea0b0d1e99f957fa626499c28b39fb62cef9
ELOPEZM <ELOPEZM@lafise.com>
2022-06-21 18:52:09 -0600

    Primer clase controlles y modelos

---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Core.Models
+{
+    public class Users
+    {
+        public int number {get;set;}
+        public int number_intern {get; set;}
+        public string email {get;set;}
+        public string nickname {get; set;}
+    }
+}
